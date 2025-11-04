74b27c61a7149ad2d2908b2ccfc1a5041b94dc71
Jinhyeon Kim <25wm2429@student.gs.chiba-u.jp>
2025-11-04 15:16:07 +0900

    all good

--- setup.py
+++ setup.py
@@ -2,15 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='kimuni',
-    version="0.0.1", 
-    description="test", 
-    author='KIM JINHYUN', 
-    packages=find_packages()
-#    license='MIT'
-)
-
-setup(
-    name='kimuni',
     version='0.0.0',
     description="description", 
     author='KIM JINHYUN', 
