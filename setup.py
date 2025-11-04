14d57fb087a70e81f18486569be9e5bd5db71810
Jinhyeon Kim <25wm2429@student.gs.chiba-u.jp>
2025-11-04 15:18:40 +0900

    all good

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'kimuni': ['../examples/*', '../examples/**/*'],
+        'kimuni': ['./examples/*', './examples/**/*'],
     },
 )
