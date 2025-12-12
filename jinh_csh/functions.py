61cd8a28274d56b8913026d1177dee43138a25d2
Jinhyeon Kim <25wm2429@student.gs.chiba-u.jp>
2025-12-12 14:57:42 +0900

    test

--- jinh_csh/functions.py
+++ jinh_csh/functions.py
@@ -1,12 +1,15 @@
-import hydra
-import importlib.resources as res
+# import hydra
+# import importlib.resources as res
 
-@hydra.main(config_path="data", config_name="T14_base", version_base=None)
-def config_base(cfg):
-    if hasattr(cfg, "BT"):
-        print("[Structure] ", cfg.BT)
+# @hydra.main(config_path="data", config_name="T14_base", version_base=None)
+# def config_base(cfg):
+#     if hasattr(cfg, "BT"):
+#         print("[Structure] ", cfg.BT)
 
-def data_lib(fname):
-    d = res.files("jinh_csh.data")
-    infile = d.joinpath(fname)
-    return infile
+# def data_lib(fname):
+#     d = res.files("jinh_csh.data")
+#     infile = d.joinpath(fname)
+#     return infile
+
+def test():
+    return 0
