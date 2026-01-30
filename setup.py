ede8a6aed33e5bf683ae408d52e328a81adf04e7
Jinhyeon Kim <25wm2429@student.gs.chiba-u.jp>
2026-01-30 18:34:42 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     include_package_data=True,
-    package_data={
-        "kimuni": ["tools/*", "cshmd/data/**/*"]
     },
 )
